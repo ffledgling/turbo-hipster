cd9c720404f5e1d33d244292a4f9c1e2430bf0e7
Anhad Jai Singh <ffledgling@gmail.com>
2014-01-26 15:54:29 +0530

    class variables

--- DBSystem.h
+++ DBSystem.h
@@ -3,14 +3,22 @@
 #include <string>
 
 using namespace std;
+typedef long long int LL;
 
 class DBSystem
 {
+        private:
+            long long int page_size;
+            long long int num_pages;
+            string path;
+            vector < string > tables;
+            map < string, vector < pair<string, string> > > attributes;
+
 	public:
-	void readConfig(string str);
-	void populateDBInfo();
-	string getRecord(string tableName, int recordId);
-	void insertRecord(string tableName, string record);
+            void readConfig(string str);
+            void populateDBInfo();
+            string getRecord(string tableName, int recordId);
+            void insertRecord(string tableName, string record);
 };
 
 #endif
